7032e8896240aabef03727afb7a72267dbe881ad
Karen <karenperarnau@gmail.com>
2023-12-11 12:55:16 -0430

    Update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,11 @@ const Header = () => {
     <header id="about" className="page_header">
       <h1 className="header_title">Hi, I'm Karen Varela</h1>
       <p className="header_introduction">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates,
-        explicabo quos doloremque
+        🚀 Hello, World! I'm Karen🌟 I’m a 22-year-old on a mission to turn
+        passion into profession. Self-taught and hungry to contribute, I'm
+        actively seeking my debut as a programmer. Dive into my digital
+        playground, where logic meets creativity. Let's code a brighter future
+        together! 💻✨
       </p>
     </header>
   );
