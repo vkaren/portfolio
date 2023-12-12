846af8e0521c2c7311939a2e70379ea1ae64c3a0
Karen <karenperarnau@gmail.com>
2023-12-12 16:55:44 -0430

    Update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,12 @@ import "./styles.css";
 const Header = () => {
   return (
     <header id="about" className="page_header">
-      <h1 className="header_title">Hi, I'm Karen Varela</h1>
+      <h1 className="header_title">🚀 Hello, World! I'm Karen🌟</h1>
       <p className="header_introduction">
-        🚀 Hello, World! I'm Karen🌟 I’m a 22-year-old on a mission to turn
-        passion into profession. Self-taught and hungry to contribute, I'm
-        actively seeking my debut as a programmer. Dive into my digital
-        playground, where logic meets creativity. Let's code a brighter future
-        together! 💻✨
+        I’m a 22-year-old on a mission to turn passion into profession.
+        Self-taught and hungry to contribute, I'm actively seeking my debut as a
+        programmer. Dive into my digital playground, where logic meets
+        creativity. Let's code a brighter future together! 💻✨
       </p>
     </header>
   );
