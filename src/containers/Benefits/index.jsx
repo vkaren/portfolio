d18bc333f5bd324a72c2b2475e582006e10c8cd1
karen <karenperarnau@gmail.com>
2024-03-25 19:11:03 -0300

    Update

--- src/containers/Benefits/index.jsx
+++ src/containers/Benefits/index.jsx
@@ -6,34 +6,28 @@ const Benefits = () => {
 
   return (
     <section className="benefits_section">
-      <div className="benefits__my-picture"></div>
+      <h2 className="benefits__heading">{t("main.benefits.heading")}</h2>
+      <p className="benefits__text">{t("main.benefits.content")}</p>
 
-      <div className="benefits__content">
-        <h2 className="benefits__content_heading">
-          {t("main.benefits.heading")}
-        </h2>
-        <p className="benefits__content_text">{t("main.benefits.content")}</p>
+      <ul className="benefits__list">
+        <li className="list__benefit">
+          <h3 className="benefit__heading">
+            {t("main.benefits.list.expertise.heading")}
+          </h3>
+          <p className="benefit__text">
+            {t("main.benefits.list.expertise.content")}
+          </p>
+        </li>
 
-        <ul className="benefits__content_list">
-          <li className="list__benefit">
-            <h3 className="benefit__heading">
-              {t("main.benefits.list.expertise.heading")}
-            </h3>
-            <p className="benefit__text">
-              {t("main.benefits.list.expertise.content")}
-            </p>
-          </li>
-
-          <li className="list__benefit">
-            <h3 className="benefit__heading">
-              {t("main.benefits.list.quality.heading")}
-            </h3>
-            <p className="benefit__text">
-              {t("main.benefits.list.quality.content")}
-            </p>
-          </li>
-        </ul>
-      </div>
+        <li className="list__benefit">
+          <h3 className="benefit__heading">
+            {t("main.benefits.list.quality.heading")}
+          </h3>
+          <p className="benefit__text">
+            {t("main.benefits.list.quality.content")}
+          </p>
+        </li>
+      </ul>
     </section>
   );
 };
