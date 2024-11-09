5af64a1fd8bae4e084876443ec2d7556089b88ea
Karen <karenperarnau@gmail.com>
2024-11-08 21:45:57 -0300

    responsive header

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-// import { ThemeProvider } from "@context/themeContext";
-import Navbar from "@components/Navbar";
+import Navbar from "@containers/Navbar";
+import Header from "@containers/Header";
 import About from "@containers/About";
 import Projects from "@containers/Projects";
 import Contact from "@containers/Contact";
-import Header from "@containers/Header";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-white text-black font-mono">
       <Navbar />
-      <main className="max-w-6xl mx-auto p-8">
+      <main className="flex flex-col gap-8 max-w-6xl mx-auto p-8">
         <Header />
         <About />
         <Projects />
--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section className="max-w-[900px] mb-12 mx-auto flex items-center justify-between">
+    <section className="flex flex-col sm:flex-row sm:justify-center lg:flex-row-reverse lg:items-end lg:justify-end lg:gap-12">
       <div className="max-w-96">
-        <h1 className="text-6xl tracking-wide mb-4 font-medium">
+        <h1 className="text-5xl sm:text-6xl tracking-wide mb-4 font-medium">
           hello
           <br />
           world_
         </h1>
         <p className="text-2xl">I&apos;m Karen, a full stack developer</p>
       </div>
-      <div className="w-96 h-96 border border-gray-600">
+      <div className="w-48 h-48 sm:w-56 sm:h-56 lg:w-64 lg:h-64 self-end sm:self-auto border border-gray-600">
         {/* <Image
         src="/placeholder.svg"
         alt="Karen's profile"
